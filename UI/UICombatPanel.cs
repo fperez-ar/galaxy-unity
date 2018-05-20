e2545bdde278389be7228d44eddfe04e6e2099a0
fperez-ar <fperez-ar@gmail.com>
2018-05-20 19:49:36 -0300

    Moved and renamed CombatPanel to ui/uicombatpanel

--- UI/UICombatPanel.cs
+++ UI/UICombatPanel.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CombatPanel : MonoBehaviour {
+public class UICombatPanel : MonoBehaviour {
 
 	public Text plyRace;
 	public Text plyCombatStrength;
